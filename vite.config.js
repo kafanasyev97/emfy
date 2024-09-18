27b7ccde0530ea36aba6f0f6ed90fa6c78693f6d
Kirill <kirillmas222@gmail.com>
2024-09-19 01:35:36 +0400

    README

--- vite.config.js
+++ vite.config.js
@@ -7,14 +7,5 @@ export default defineConfig({
     host: true,
     port: 5173,
   },
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'https://afanasyevkirill97.amocrm.ru',
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/api/, ''),
-  //     },
-  //   },
-  // },
   plugins: [react()],
 })
